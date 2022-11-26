9e4a71796d410762726dac622870d0ed4d8a84fd
박세연 <wsx4379@naver.com>
2022-11-26 22:47:46 +0900

    main, search

--- app/src/main/java/com/app/teamproject/MainActivity.java
+++ app/src/main/java/com/app/teamproject/MainActivity.java
@@ -1,21 +1,39 @@
 package com.app.teamproject;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     FloatingActionButton btnSubway, btnSetting, btnHome, btnRoad, btnStar, btnSearch;
+    ImageView option, search;
+    TextView search_et;
     Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        option = findViewById(R.id.main_option);
+        search = findViewById(R.id.main_search);
+        search_et = findViewById(R.id.main_search_et);
+
+        option.setOnClickListener(this);
 
         btnSubway = findViewById(R.id.fab_subway);
 
@@ -53,11 +71,62 @@ public class MainActivity extends AppCompatActivity {
                 isAllFabsVisible = false;
             }
         });
-        btnHome.setOnClickListener(
-                view -> Toast.makeText(MainActivity.this, "Person Added", Toast.LENGTH_SHORT).show());
+        btnHome.setOnClickListener(this);
+        btnSetting.setOnClickListener(this);
+        btnSearch.setOnClickListener(this);
+        btnStar.setOnClickListener(this);
+        btnRoad.setOnClickListener(this);
+    }
 
-        btnSetting.setOnClickListener(
-                view -> Toast.makeText(MainActivity.this, "Alarm Added", Toast.LENGTH_SHORT
-                ).show());
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.fab_home:
+                break;
+            case R.id.fab_setting:
+                break;
+            case R.id.fab_search:
+                intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.fab_star:
+                break;
+            case R.id.fab_road:
+                break;
+            case R.id.main_option:
+                this.registerForContextMenu(option);
+                openContextMenu(option);
+                unregisterForContextMenu(option);
+                break;
+        }
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mi = getMenuInflater();
+        if(v == option){
+            mi.inflate(R.menu.menu, menu);
+        }
+
+    }
+
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemLanguage:
+                Toast.makeText(this, "language", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itemAsk:
+                Toast.makeText(this, "ask", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itemSetting:
+                Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itemHelp:
+                Toast.makeText(this, "help", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+
     }
 }
